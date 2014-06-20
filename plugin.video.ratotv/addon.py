89d3bd2f3bf15d0522428c76f6fe4fc3e1a90853
Carlos Correia <MafaStudios@gmail.com>
2014-06-21 00:37:11 +0100

    small cleanup

--- plugin.video.ratotv/addon.py
+++ plugin.video.ratotv/addon.py
@@ -272,16 +272,12 @@ def stream_qualidade(url,name,iconimage):
 			print titles,url_movie_file,subs
 			if selfAddon.getSetting('qualidade-auto') == "false":
 				choose=escolher_qualidade('Seleccione a qualidade',titles)
-				if choose > -1:
-						linkescolha=player_rato(url_movie_file[choose]+ '|host=ratotv.com&referer=' + url,subs,name,url,iconimage,'',None,None)
-			else:
-				linkescolha=player_rato(url_movie_file[0]+ '|host=ratotv.com&referer=' + url,subs,name,url,iconimage,'',None,None)
+				if choose > -1: linkescolha=player_rato(url_movie_file[choose]+ '|host=ratotv.com&referer=' + url,subs,name,url,iconimage,'',None,None)
+			else: linkescolha=player_rato(url_movie_file[0]+ '|host=ratotv.com&referer=' + url,subs,name,url,iconimage,'',None,None)
 		else:
-			if rss_source:
-				hash_movie = re.compile('<location>ratotv*(.+?)</location>').findall(rss_source)[0].replace("*","")
+			if rss_source: hash_movie = re.compile('<location>ratotv*(.+?)</location>').findall(rss_source)[0].replace("*","")
 			else: ok=mensagemok('RatoTV','Não conseguiu obter a hash.')
-			try:
-				decoded_url = resolver_externos(hash_movie)
+			try: decoded_url = resolver_externos(hash_movie)
 			except: ok=mensagemok('RatoTV','Não conseguiu resolver a hash.')
 			try:
 					subs = re.compile('<captions.files>../(.+?)</captions.files>').findall(rss_source)
